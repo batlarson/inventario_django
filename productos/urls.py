ff87526eecf78dd2d7f1597dd2a0eb25d6c3e6ab
Ibon Mugica <iboedu@hotmail.com>
2026-03-09 04:44:12 +0100

    CRUD completo y mensajes, añado login

--- productos/urls.py
+++ productos/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('listado/', views.listado_productos, name='listado'),
     path('nuevo/', views.crear_producto, name='crear'),
     path('editar/<int:id_producto>/', views.editar_producto, name='editar'),
+    path('eliminar/<int:id_producto>/', views.eliminar_producto, name='eliminar')
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import Producto
 from .forms import ProductoForm
 from .ia_logic import predecir_reabastecimiento
@@ -21,6 +23,7 @@ def listado_productos(request):
     }
     return render(request, 'productos/lista.html', contexto)
 
+@login_required
 def crear_producto(request):
     if request.method == 'POST':
         form = ProductoForm(request.POST)
@@ -31,6 +34,7 @@ def crear_producto(request):
         form = ProductoForm()
     return render(request, 'productos/crear.html', {'form': form})
 
+@login_required
 def editar_producto(request, id_producto):
     producto = get_object_or_404(Producto, id=id_producto)
     
@@ -43,3 +47,14 @@ def editar_producto(request, id_producto):
         form = ProductoForm(instance=producto)
     
     return render(request, 'productos/crear.html', {'form': form, 'editando': True})
+
+@login_required
+def eliminar_producto(request, id_producto):
+    producto = get_object_or_404(Producto, id=id_producto)
+    
+    if request.method == 'POST':
+        producto.delete()
+        messages.success(request, f'El producto "{producto.nombre}" ha sido eliminado correctamente.')
+        return redirect('listado')
+
+    return render(request, 'productos/confirmar_eliminar.html', {'producto': producto})
