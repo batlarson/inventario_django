6a70ab09dbb5c16ec5e9baea6e2512aa9e55ce58
Ibon Mugica <iboedu@hotmail.com>
2026-03-15 07:14:46 +0100

    Errores y validaciones con mensaje

--- productos/forms.py
+++ productos/forms.py
@@ -5,3 +5,15 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['nombre', 'stock', 'precio', 'perecedero', 'categoria']
+    
+    def clean_precio(self):
+        precio = self.cleaned_data.get('precio')
+        if precio <= 0:
+            raise forms.ValidationError("¡El precio debe ser mayor que cero! No estamos regalando nada.")
+        return precio
+    
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if len(nombre) < 3:
+            raise forms.ValidationError("El nombre es demasiado corto, pon algo más descriptivo.")
+        return nombre
