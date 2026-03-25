e6b944286f517adb203042b73cf0a9bf00356cb0
Ibon Mugica <iboedu@hotmail.com>
2026-03-25 06:28:46 +0100

    API: Serializer actualizado y Perfil registrado en Admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Producto, Categoria, Historial
+from .models import Producto, Categoria, Historial, Perfil
 
 admin.site.register(Producto)
 admin.site.register(Categoria)
 admin.site.register(Historial)
+admin.site.register(Perfil)
--- productos/serializers.py
+++ productos/serializers.py
@@ -3,9 +3,10 @@ from .models import Producto, Categoria
 
 class ProductoSerializer(serializers.ModelSerializer):
     categoria_nombre = serializers.ReadOnlyField(source='categoria.nombre')
+    imagen = serializers.ImageField(required=False, allow_null=True)
     class Meta:
         model = Producto
-        fields = ['id', 'nombre', 'stock', 'precio', 'categoria', 'categoria_nombre']
+        fields = ['id', 'nombre', 'stock', 'precio', 'categoria', 'categoria_nombre', "imagen"]
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
