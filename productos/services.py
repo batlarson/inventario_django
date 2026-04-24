69c16d17a5bbce47db5eb8d80616ef856dea200a
Ibon Mugica <iboedu@hotmail.com>
2026-04-24 06:38:00 +0200

    ACID en funcion para proteger en caso de datos masivos

--- productos/services.py
+++ productos/services.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.db.models import F
 from .models import Producto
 
@@ -20,10 +21,14 @@ class ProductoService:
     @staticmethod
     def aplicar_inflacion_masiva(porcentaje=10):
         """
-        Aumenta el precio de todos los productos usando una sola query SQL.
+        Aumenta el precio de todos los productos usando una sola query SQL (protegido por transaction).
         """
         factor = 1 + (porcentaje / 100.0) # Si es 10, el factor es 1.10
-        
-        # Procesamiento masivo con F()
-        filas_actualizadas = Producto.objects.update(precio=F('precio') * factor)
-        return filas_actualizadas
+
+        try:
+            with transaction.atomic():        
+                # Procesamiento masivo con F()    Como uso transaction deberia ser "Todo o Nada"
+                filas_actualizadas = Producto.objects.update(precio=F('precio') * factor)
+            return filas_actualizadas
+        except Exception as e:
+            raise Exception(f"Fallo masivo. Se ha cancelado la operación: {e}")
