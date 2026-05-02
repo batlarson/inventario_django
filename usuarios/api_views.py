99af3f6770ce5410c38acd38dbc03f32a88506ef
Ibon Mugica <iboedu@hotmail.com>
2026-05-02 21:16:44 +0200

    Añadir JWT a nuestro usuario

--- usuarios/api_views.py
+++ usuarios/api_views.py
@@ -1,9 +1,12 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializer import PerfilSerializer
 
 class PerfilUsuarioAPIView(generics.RetrieveUpdateAPIView):
     serializer_class = PerfilSerializer
+
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from . import views
 from .api_views import PerfilUsuarioAPIView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('perfil/', views.editar_perfil, name='perfil'),
     path('api/perfil/', PerfilUsuarioAPIView.as_view(), name='api-perfil'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
