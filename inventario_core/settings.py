8d60e8c6a2060c1a28c346691cddc21168688b05
Ibon Mugica <iboedu@hotmail.com>
2026-05-10 21:19:16 +0200

    Fix 8 recuperar css para produccion

--- inventario_core/settings.py
+++ inventario_core/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-#STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LOGIN_URL = 'login'
