bedbe980e276d789ebe17cebf61ff18aa93a1bce
Ibon Mugica <iboedu@hotmail.com>
2026-05-10 21:41:40 +0200

    Fix trusted origins

--- inventario_core/settings.py
+++ inventario_core/settings.py
@@ -30,6 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
 
 # Application definition
 
