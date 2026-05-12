109ca72d8f3cf2d3a28b429bd4301833ffca8078
Ibon Mugica <iboedu@hotmail.com>
2026-05-12 23:13:29 +0200

    Vuelta

--- inventario_core/settings.py
+++ inventario_core/settings.py
@@ -139,8 +139,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LOGIN_URL = 'login'
@@ -194,26 +193,6 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 # Lo pondremos en la raíz del proyecto, junto a manage.py
 LOG_FILE_PATH = os.path.join(BASE_DIR, 'tienda_errores.log')
 
-HANDLERS_LIST = ['console']
-HANDLERS_CONFIG = {
-    # Esto manda los mensajes a la consola (para cuando programas)
-    'console': {
-        'class': 'logging.StreamHandler',
-        'formatter': 'profesional',
-    },
-}
-
-if DEBUG:
-    HANDLERS_LIST.append('file')
-    HANDLERS_CONFIG['file'] = {
-        # Esto es la CAJA NEGRA: guarda en un archivo de texto
-        'level': 'WARNING', # Solo guarda de WARNING para arriba (Error, Critical)
-        'class': 'logging.FileHandler',
-        'filename': LOG_FILE_PATH,
-        'formatter': 'profesional',
-    }
-
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -224,7 +203,20 @@ LOGGING = {
             'style': '{',
         },
     },
-    'handlers': HANDLERS_CONFIG,
+    'handlers': {
+        # Esto manda los mensajes a la consola (para cuando programas)
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'profesional',
+        },
+        # Esto es la CAJA NEGRA: guarda en un archivo de texto
+        'file': {
+            'level': 'WARNING', # Solo guarda de WARNING para arriba (Error, Critical)
+            'class': 'logging.FileHandler',
+            'filename': LOG_FILE_PATH,
+            'formatter': 'profesional',
+        },
+    },
     'loggers': {
         # Atrapamos todos los logs de nuestra app
         '': { 
